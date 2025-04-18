e7495a7c5031790c5051ce00c53fd174515d870a
Krishna019 <krishnankundan4845@gmail.com>
2025-04-18 18:42:26 +0530

    ConcurrentSkipListMapDemo

---
+++ src/Map/ConcurrentSkipListMapDemo.java
@@ -0,0 +1,9 @@
+package Map;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ConcurrentSkipListMapDemo {
+    public static void main(String[] args) {
+        ConcurrentSkipListMap<String,Integer> map = new ConcurrentSkipListMap<>();
+    }
+}
